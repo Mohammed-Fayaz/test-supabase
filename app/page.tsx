5548cc3b16860cf361976966479660f574cd766c
Fayaz <fayazsalim@gmail.com>
2023-06-18 20:37:49 +0530

    Adding types for supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import { supabase } from "../lib/supabaseClient";
 
 export default async function Home() {
-  let { data } = await supabase.from("countries").select();
+  let { data: countries, error } = await supabase.from("countries").select();
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center text-5xl min-h-screen">
+        Error!
+      </div>
+    );
+  }
 
   return (
     <ul>
-      {data?.map((country) => (
+      {countries?.map((country) => (
         <li key={country.id} className="pl-2 text-lg">
           {country.id}. {country.name}
         </li>
