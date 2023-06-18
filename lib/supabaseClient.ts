577de80ec850f9ceba16dd0486f22d2c25eb722b
Fayaz <fayazsalim@gmail.com>
2023-06-18 20:02:32 +0530

    Adding in environment variables

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://yrkouqxtdxbxdsvdtsku.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlya291cXh0ZHhieGRzdmR0c2t1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcwOTM3MDcsImV4cCI6MjAwMjY2OTcwN30.QBHLZCqYT5uaHRDRyrgQEA4oYpPRkZO0MYT_IRb-N5M"
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
 );
